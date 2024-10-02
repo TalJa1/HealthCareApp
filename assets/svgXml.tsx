159c611f55d14925f257c4054be323d6d4571806
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-02 15:26:31 +0700

    Refactor SVG icons to use template literals for improved readability and maintainability

---
+++ assets/svgXml.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable quotes */
+import React from 'react';
+import {SvgXml} from 'react-native-svg';
+
+export const test = (w?: any, h?: any, color?: any) => {
+  const xml = ``;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
+export const homeIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 18V15" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.07 2.82009L3.14002 8.37009C2.36002 8.99009 1.86002 10.3001 2.03002 11.2801L3.36002 19.2401C3.60002 20.6601 4.96002 21.8101 6.40002 21.8101H17.6C19.03 21.8101 20.4 20.6501 20.64 19.2401L21.97 11.2801C22.13 10.3001 21.63 8.99009 20.86 8.37009L13.93 2.83009C12.86 1.97009 11.13 1.97009 10.07 2.82009Z" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
+export const listIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22.2633 6.76001L18.8933 20.29C18.6533 21.3 17.7533 22 16.7133 22H3.57326C2.06326 22 0.983275 20.5199 1.43328 19.0699L5.64327 5.55005C5.93327 4.61005 6.80328 3.95996 7.78328 3.95996H20.0833C21.0333 3.95996 21.8233 4.53997 22.1533 5.33997C22.3433 5.76997 22.3833 6.26001 22.2633 6.76001Z" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10"/>
+<path d="M16.3333 22H21.1133C22.4033 22 23.4133 20.91 23.3233 19.62L22.3333 6" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10.0133 6.38L11.0533 2.06006" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.7133 6.39001L17.6533 2.05005" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.03333 12H16.0333" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.03333 16H15.0333" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
+export const newsIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.6667 22C18.1895 22 22.6667 17.5228 22.6667 12C22.6667 6.47715 18.1895 2 12.6667 2C7.14384 2 2.66669 6.47715 2.66669 12C2.66669 17.5228 7.14384 22 12.6667 22Z" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.66666 3H9.66666C7.71666 8.84 7.71666 15.16 9.66666 21H8.66666" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15.6667 3C17.6167 8.84 17.6167 15.16 15.6667 21" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3.66669 16V15C9.50669 16.95 15.8267 16.95 21.6667 15V16" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M3.66669 9.0001C9.50669 7.0501 15.8267 7.0501 21.6667 9.0001" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
+
+export const userIcon = (w?: any, h?: any, color?: any) => {
+  const xml = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.1601 10.87C12.0601 10.86 11.9401 10.86 11.8301 10.87C9.45006 10.79 7.56006 8.84 7.56006 6.44C7.56006 3.99 9.54006 2 12.0001 2C14.4501 2 16.4401 3.99 16.4401 6.44C16.4301 8.84 14.5401 10.79 12.1601 10.87Z" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.15997 14.56C4.73997 16.18 4.73997 18.82 7.15997 20.43C9.90997 22.27 14.42 22.27 17.17 20.43C19.59 18.81 19.59 16.17 17.17 14.56C14.43 12.73 9.91997 12.73 7.15997 14.56Z" stroke="${
+    color ?? '#8098F9'
+  }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+  return <SvgXml xml={xml} width={w} height={h} />;
+};
