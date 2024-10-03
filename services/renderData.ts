12147d4dbfaba1877c2bbf501a55d5b2183f3ad4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-03 12:36:56 +0700

    Refactor styles to add Irritable Bowel Syndrome (IBS) data in HomeLearnMoreData array

--- services/renderData.ts
+++ services/renderData.ts
@@ -25,4 +25,15 @@ export const DiseaseCategoriesTabs = [
   },
 ];
 
-export const HomeLearnMoreData = [];
+export const HomeLearnMoreData = [
+  {
+    disease: 'Irritable Bowel Syndrome (IBS)',
+    img: require('../assets/home/stomach.png'),
+    description:
+      'Irritable Bowel Syndrome is a gastrointestinal disorder that causes abdominal pain, bloating, and changes in bowel habits, such as diarrhea or constipation. It impacts the function of the digestive system without causing structural damage.',
+    thingsToDo:
+      'Eat small, frequent meals, follow a low-FODMAP diet, and manage stress levels.',
+    thingsNotToDo: 'Consume fatty, fried foods, overeat, or neglect hydration.',
+    excercise: [{}],
+  },
+];
