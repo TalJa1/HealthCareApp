7d9b3fbd418abdab700d571c4981fb99e0bff6bb
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-03 13:46:26 +0700

    Refactor styles to add doubleSaveIcon and backIcon components

--- views/home/Detail.tsx
+++ views/home/Detail.tsx
@@ -1,16 +1,60 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
-import {main} from '../../services/styleSheets';
+import {main, vh, vw} from '../../services/styleSheets';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {HomeLearnMoreData} from '../../services/renderData';
+import {backIcon, doubleSaveIcon} from '../../assets/svgXml';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+type DetailRouteParams = {
+  Detail: {
+    dataIndex: number;
+  };
+};
 
 const Detail = () => {
+  const route = useRoute<RouteProp<DetailRouteParams, 'Detail'>>();
+  const {dataIndex} = route.params;
+  const renderData = HomeLearnMoreData[dataIndex];
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View>
-          <Text>detail</Text>
-        </View>
+        <Header />
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const Header: React.FC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        paddingHorizontal: vw(5),
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: vh(2),
+      }}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        {backIcon(vw(7), vw(7))}
+      </TouchableOpacity>
+      <Text style={styles.headerTxt}>Detail</Text>
+      <View>{doubleSaveIcon(vw(7), vw(7))}</View>
+    </View>
   );
 };
 
@@ -18,4 +62,9 @@ export default Detail;
 
 const styles = StyleSheet.create({
   container: main,
+  headerTxt: {
+    color: '#344054',
+    fontSize: 20,
+    fontWeight: '600',
+  },
 });
