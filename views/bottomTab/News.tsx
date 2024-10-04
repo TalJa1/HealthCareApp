d7cad24ce14c9baccc016869ee6187a96acefcb8
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-04 16:35:17 +0700

    Refactor News.tsx to include useStatusBar hook and improve layout

--- views/bottomTab/News.tsx
+++ views/bottomTab/News.tsx
@@ -1,19 +1,42 @@
+/* eslint-disable react-native/no-inline-styles */
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {main} from '../../services/styleSheets';
+import {main, vh, vw} from '../../services/styleSheets';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useStatusBar from '../../services/useStatusBar';
+import {menuIcon} from '../../assets/svgXml';
 
 const News = () => {
   useStatusBar('#EAECF5');
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View>
-          <Text>News</Text>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={{flex: 1, paddingHorizontal: vw(5), marginBottom: vh(2)}}>
+          <Header />
+          <MainContent />
         </View>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const MainContent: React.FC = () => {
+  return <View></View>;
+};
+
+const Header: React.FC = () => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: vh(2),
+      }}>
+      <Text style={{color: '#4E5BA6', fontSize: 20, fontWeight: '600'}}>
+        News
+      </Text>
+      {menuIcon(vw(8), vw(8))}
+    </View>
   );
 };
 
