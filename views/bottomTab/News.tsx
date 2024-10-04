80304b6024fb9e4b9de01fa99cecf7bf6eb21680
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-10-04 15:46:41 +0700

    Refactor News.tsx to include useStatusBar hook

--- views/bottomTab/News.tsx
+++ views/bottomTab/News.tsx
@@ -2,8 +2,10 @@ import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {main} from '../../services/styleSheets';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import useStatusBar from '../../services/useStatusBar';
 
 const News = () => {
+  useStatusBar('#EAECF5');
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
